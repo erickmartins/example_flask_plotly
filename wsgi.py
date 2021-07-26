29600d1fc78f3a4def236f6f28fe4beea44a09f7
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-26 11:41:10 -0400

    changed structure

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
-from app import app
+#!/usr/bin/env python
 
-if __name__ == "__main__":
-    app.run()
+import sys
+import site
 
+site.addsitedir('/home/ratame/miniconda3/envs/dashboard/lib/python3.9/site-packages/')
+
+sys.path.insert(0, '/home/ratame/example_flask_plotly')
+
+from app import app as application
