c2df740e65952d778bd9f66ace63f8d9da718400
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-27 13:06:26 -0400

    testing new template

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,11 @@
 import sys
 import site
 
-site.addsitedir('/home/ratame/miniconda3/envs/dashboard/lib/python3.9/site-packages/')
+site.addsitedir('/var/www/example_flask_plotly/dashboard/lib/python3.6/site-packages/')
+site.addsitedir('/var/www/example_flask_plotly/dashboard/lib64/python3.6/site-packages/')
 
-sys.path.insert(0, '/home/ratame/example_flask_plotly')
+
+sys.path.insert(0, '/var/www/example_flask_plotly')
+
 
 from app import app as application
