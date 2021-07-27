ae377f05045a8487d33583c7ce77839c8718ccc7
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-27 14:11:02 -0400

    subplots working

--- app/__init__.py
+++ app/__init__.py
@@ -8,21 +8,23 @@ import numpy as np
 
 app = Flask(__name__)
 
+def get_data(n):
+    x = np.linspace(0, 1, n)
+    y = np.random.randn(n)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    return df
+
 def create_plot():
     N = 40
-    x = np.linspace(0, 1, N)
-    y = np.random.randn(N)
-    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
-    fig = make_subplots(cols=2)
-    data = [
-        go.Bar(
-            x=df['x'], # assign x as the dataframe column 'x'
-            y=df['y']
-        )
-    ]
-    fig.add_bar(data, row=1, col=1)
-    fig.add_bar(data, row=1, col=2)
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    df1 = get_data(N)
+    df2 = get_data(N)
+    df3 = get_data(N)
+    fig = make_subplots(rows=2, cols=2,specs=[[{}, {}],[{'colspan': 2}, None]])    
+    fig.add_bar(x=df1['x'],y=df1['y'], row=1, col=1)
+    fig.add_bar(x=df2['x'],y=df2['y'], row=1, col=2)
+    fig.add_bar(x=df3['x'],y=df3['y'], row=2, col=1)
+    fig.update_layout(height=800)
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
 @app.route('/')
