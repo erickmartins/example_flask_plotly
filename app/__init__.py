6f62cdf580120398fdbec3b72f4e09368ed897d0
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-27 13:52:39 -0400

    trying subplots

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import pandas as pd
 import json
 import plotly
+from plotly.subplots import make_subplots
 import plotly.graph_objs as go
 import numpy as np
 
@@ -12,19 +13,20 @@ def create_plot():
     x = np.linspace(0, 1, N)
     y = np.random.randn(N)
     df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+    fig = make_subplots(cols=2)
     data = [
         go.Bar(
             x=df['x'], # assign x as the dataframe column 'x'
             y=df['y']
         )
     ]
+    fig.add_bar(data, row=1, col=1)
+    fig.add_bar(data, row=1, col=2)
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
 @app.route('/')
 def index():
     bar = create_plot()
-    bar2 = create_plot()
-    bar3 = create_plot()
-    return render_template('index.html', plot=bar, plot2=bar2, plot3=bar3)
+    return render_template('index.html', plot=bar)
 
