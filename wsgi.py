47893499b7c455e022ff986f67c6880b80ca620d
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-30 13:58:47 -0400

    removed linebreaks just in case

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,8 @@
 import sys
 import site
 
-site.addsitedir('/var/www/example_flask_plotly/\
-                dashboard/lib/python3.6/site-packages/')
-site.addsitedir('/var/www/example_flask_plotly/\
-                dashboard/lib64/python3.6/site-packages/')
+site.addsitedir('/var/www/example_flask_plotly/dashboard/lib/python3.6/site-packages/')
+site.addsitedir('/var/www/example_flask_plotly/dashboard/lib64/python3.6/site-packages/')
 
 sys.path.insert(0, '/var/www/example_flask_plotly')
 
