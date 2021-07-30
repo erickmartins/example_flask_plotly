a9b219dac6fac80805efda0f670709f77383c4c2
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-30 13:57:50 -0400

    put import where it was

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 """WSGI config adding site packages and path."""
 import sys
 import site
-from app import app as application
 
 site.addsitedir('/var/www/example_flask_plotly/\
                 dashboard/lib/python3.6/site-packages/')
@@ -9,3 +8,5 @@ site.addsitedir('/var/www/example_flask_plotly/\
                 dashboard/lib64/python3.6/site-packages/')
 
 sys.path.insert(0, '/var/www/example_flask_plotly')
+
+from app import app as application
